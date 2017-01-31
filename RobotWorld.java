0de20a53dc07c20ec18925485148a93b184351a6
Denon <lucival1@gmail.com>
2017-01-31 12:43:28 +0000

    Update RobotWorld.java

--- RobotWorld.java
+++ RobotWorld.java
@@ -1,27 +1,35 @@
 import processing.core.PApplet;
+import processing.core.PFont;
 
 public class RobotWorld extends PApplet{
-  Robot[] robot;
+	Robot[] robot;
+	Interface i;
 
   public static void main(String[] args){
-    PApplet.main("RobotWorld"); //create the windows (world) where objects live
+	  PApplet.main("RobotWorld"); //create the windows (world) where objects live
   }
 
   public void settings(){
-    size(1000, 500);
-    robot = new Robot[3];
-    robot[0] = new Robot(this, color(255, 0, 0), 100, 100, 3, 3f, 90);
-    robot[1] = new Robot(this, color(0, 255, 0), 200, 200, 5, 4f, 0);
-    robot[2] = new Robot(this, color(0, 0, 255), 200, 300, 5, 5f, 270);
+	  size(1000, 500);
+	  robot = new Robot[3];
+	  robot[0] = new Robot(this, color(255, 0, 0), 100, 100, 3, 3f, 90);
+	  robot[1] = new Robot(this, color(0, 255, 0), 200, 200, 5, 4f, 0);
+	  robot[2] = new Robot(this, color(0, 0, 255), 200, 300, 5, 5f, 270);
   }
 
   public void setup(){
+	  PFont f = createFont("Arial", 16, true);
+	  i = new Interface(this, f);
   }
 
   public void draw(){
-    background(255);
-    robot[0].randomWalk();
-    robot[1].patrol();
-    robot[2].stroll();
-  }
+	  background(255);
+	  i.printInterface();
+	  for (int i = 0; i < 3; i++){
+		  robot[i].stopCheck();
+	  }
+	  robot[0].randomWalk();
+	  robot[1].patrol();
+	  robot[2].stroll();
+  }  
 }
