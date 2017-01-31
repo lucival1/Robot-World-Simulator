a7e41650dc2b8b00261420756cf3ee8959608f0a
Denon <lucival1@gmail.com>
2017-01-31 12:31:56 +0000

    Update RobotWorld.java

--- RobotWorld.java
+++ RobotWorld.java
@@ -9,27 +9,19 @@ public class RobotWorld extends PApplet{
 
   public void settings(){
     size(1000, 500);
+    robot = new Robot[3];
+    robot[0] = new Robot(this, color(255, 0, 0), 100, 100, 3, 3f, 90);
+    robot[1] = new Robot(this, color(0, 255, 0), 200, 200, 5, 4f, 0);
+    robot[2] = new Robot(this, color(0, 0, 255), 200, 300, 5, 5f, 270);
   }
 
   public void setup(){
-    robot = new Robot[3];
-    robot[0] = new Robot(this, color(255, 0, 0), (float)(width/10), (float)(height/10),
-	 (float)(3*width/20), (float)(3*height/20),
-	 (float)(width/10), (float)(height/5), 5f);
-
-    robot[1] = new Robot(this, color(0, 255, 0), (float)(width/10), (float)(3*height/10),
-	 (float)(3*width/20), (float)(21*height/60),
-	 (float)(width/10), (float)(2*height/5), 5f);
-
-    robot[2] = new Robot(this, color(0, 0, 255), (float)(width/10), (float)(height/2),
-	 (float)(3*width/20), (float)(11*height/20),
-	 (float)(width/10), (float)(6*height/10), 5f);
   }
 
   public void draw(){
     background(255);
-      robot[0].bounce();
-      robot[1].patrol();
-      robot[2].stroll();
+    robot[0].randomWalk();
+    robot[1].patrol();
+    robot[2].stroll();
   }
 }
